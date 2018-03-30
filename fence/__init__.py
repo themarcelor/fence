caf734532507919954f634fff013cea19e52df38
Jesus Pedrosa <jesuspg@uchicago.edu>
2018-03-30 19:42:01 +0000

    feat(sec): fix referer

--- fence/__init__.py
+++ fence/__init__.py
@@ -195,9 +195,9 @@ def check_csrf():
         return
     # cookie based authentication
     if flask.request.method != 'GET':
-        csrf_cookie = flask.request.headers.get('x-csrf-token')
-        csrf_header = flask.request.cookies.get('csrftoken')
-        referer = flask.request.META.get('HTTP_REFERER')
+        csrf_header = flask.request.headers.get('x-csrf-token')
+        csrf_cookie = flask.request.cookies.get('csrftoken')
+        referer = flask.request.headers.get('referer')
         flask.current_app.logger.debug('HTTP REFERER ' + referer)         
         if not csrf_cookie\
         or not csrf_header\
